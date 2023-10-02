e2d9f57e895c0174839a5595780064f3a4c68a29
root <root@lab1.myguest.virtualbox.org>
2023-10-02 08:45:46 +0300

    New commit!!!

---
+++ ex.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import redis
+r=redis.Redis(host='localhost', port=6379)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Web App with Python Flask!'
+ink=0
+@app.route('/visit/<id>')
+
+def visit(id):
+	if r.get(id) is not None:
+		r.incr(id)
+	else:
+		r.set(id,1)
+	
+	return 'OK'
+@app.route('/show/<id>')
+def show(id):
+	a=r.get(id)
+	return "visits: %s." % a
+app.run(host='0.0.0.0', port=81)
