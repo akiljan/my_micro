950ca39291932d93df0e0c81a83e8845f36c81cc
akiljan <57321344+akiljan@users.noreply.github.com>
2023-10-24 17:53:22 +0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,17 +1,17 @@
 """
-high level support for doing this and that.
+high level support foRED_RED.doing this and that.
 """
 from flask import Flask
 import redis
 
-r = redis.Redis(host='redis', port=6379)
+RED_RED = redis.Redis(host='redis', port=6379)
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     """
-    high level support for doing this and that.
+    high level support foRED_RED.doing this and that.
     """
     return 'Web App with Python Flask!'
 
@@ -22,21 +22,21 @@ ink = 0
 @app.route('/visit/<tutre>')
 def visit(tutre):
     """
-    high level support for doing this and that.
+    high level support foRED_RED.doing this and that.
     """
-    if r.get(tutre) is not None:
-        r.incr(tutre)
+    if RED_RED.get(tutre) is not None:
+        RED_RED.incr(tutre)
     else:
-        r.set(tutre, 1)
+        RED_RED.set(tutre, 1)
     return 'OK'
 
 
 @app.route('/show/<tutre>')
 def show(tutre):
     """
-    high level support for doing this and that.
+    high level support foRED_RED.doing this and that.
     """
-    a = r.get(tutre)
+    a = RED_RED.get(tutre)
     return "visits: %s" % str(a, 'ascii')
 
 
