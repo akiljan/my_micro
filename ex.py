fcb89ea5b22bb595cde29cb5f880d3b02a7ee332
akiljan <57321344+akiljan@users.noreply.github.com>
2023-10-24 17:45:03 +0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,23 +1,35 @@
+"""
+high level support for doing this and that.
+"""
 from flask import Flask
 import redis
-r=redis.Redis(host='redis', port=6379)
+
+r = redis.Redis(host='redis', port=6379)
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-	return 'Web App with Python Flask!'
-ink=0
-@app.route('/visit/<id>')
+    return 'Web App with Python Flask!'
 
-def visit(id):
-	if r.get(id) is not None:
-		r.incr(id)
-	else:
-		r.set(id,1)
-	
-	return 'OK'
-@app.route('/show/<id>')
-def show(id):
-	a=r.get(id)
-	return "visits: %s" % str(a, 'ascii')
+
+ink = 0
+
+
+@app.route('/visit/<tutre>')
+def visit(tutre):
+    if r.get(tutre) is not None:
+        r.incr(tutre)
+    else:
+        r.set(tutre, 1)
+
+    return 'OK'
+
+
+@app.route('/show/<tutre>')
+def show(tutre):
+    a = r.get(tutre)
+    return "visits: %s" % str(a, 'ascii')
+
+
 app.run(host='0.0.0.0', port=81)
